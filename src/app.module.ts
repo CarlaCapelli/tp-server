259c9521c02db14962d4f9c53b238e83345059a0
Carla Capelli <carlacapelli84@gmail.com>
2023-08-10 20:42:40 -0300

    conexion mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'tu password', // poner el password
+      database: 'soat',
+      entities: [__dirname + '/../dist/**/entities/*.entity.{js,ts}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
