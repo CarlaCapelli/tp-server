34e83e8d3b317cf7b5e97f6451056a2fb8113d05
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2023-08-17 21:39:06 -0300

    Modificacion Cliente Orden

--- src/orden/entities/orden.entity.ts
+++ src/orden/entities/orden.entity.ts
@@ -1,1 +1,102 @@
-export class Orden {}
+import { Entity, PrimaryGeneratedColumn, Column,JoinColumn, ManyToOne } from 'typeorm';
+import { Cliente } from 'src/cliente/entities/cliente.entity';
+
+@Entity('orden')
+export class Orden {
+
+  @PrimaryGeneratedColumn()
+  private id: number;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  private fechaIngreso: Date;
+
+  @Column({ nullable: true })
+  private fechaRevisado: Date;
+
+  @Column({ nullable: true })
+  private fechaEntregado: Date;
+
+  @Column({ nullable: true })
+  private accesorio: string;
+
+  @Column()
+  private falla: string;
+
+  @Column({ nullable: true })
+  private informe: string;
+
+  @Column({ nullable: true })
+  private importe: number;
+
+  @Column({ default: () => 0 })
+  private estado: number;
+
+  constructor(falla: string) {
+    this.falla = falla;
+  }
+
+  public getIdOrden(): number {
+    return this.id;
+  };
+
+  public getFechaIngreso(): Date {
+    return this.fechaIngreso;
+  };
+
+  public getFechaRevisado(): Date {
+    return this.fechaRevisado;
+  };
+
+  public setFechaRevisado(fechaRevisado: Date): void {
+    this.fechaRevisado = fechaRevisado;
+  };
+
+  public getFechaEntregado(): Date {
+    return this.fechaEntregado;
+  };
+
+  public setFechaEntregado(fechaEntregado: Date): void {
+    this.fechaEntregado = fechaEntregado;
+  };
+
+  public getAccesorio():string{
+    return this.accesorio;
+  };
+
+  public setAccesorio(newAccesorio:string):void{
+    this.accesorio = newAccesorio;
+  };
+
+  public getFalla():string {
+    return this.falla;
+  };
+
+  public getInforme(): string{
+    return this.informe
+  };
+
+  public setInforme(newInforme:string):void {
+    this.informe = newInforme;
+  };
+
+  public getImporte():number {
+    return this.importe;
+  };
+
+  public setImporte(newImporte: number):void{
+    this.importe = newImporte;
+  };
+
+  public getEstado():number {
+    return this.estado;
+  };
+
+  public setEstado(newEstado:number): void {
+    this.estado = newEstado;
+  }
+
+  @ManyToOne(()=> Cliente, cliente => cliente.orden)
+  @JoinColumn({name:'id_cliente'})
+  cliente: Cliente;
+
+}
