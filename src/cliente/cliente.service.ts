5b606a0150ab794bc343ae517f64f536f9a40ab3
Facundo Mamonde <facundo.mamonde.96@gmail.com>
2023-08-24 21:33:27 -0300

    Avance en cliente

--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -1,26 +1,94 @@
-import { Injectable } from '@nestjs/common';
-import { CreateClienteDto } from './dto/create-cliente.dto';
-import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository, FindOneOptions } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ClienteDto } from './dto/cliente.dto';
+import { Cliente } from './entities/cliente.entity';
 
 @Injectable()
 export class ClienteService {
-  create(createClienteDto: CreateClienteDto) {
-    return 'This action adds a new cliente';
+  private clientes: Cliente[] = [];
+
+  constructor(
+    @InjectRepository(Cliente)
+    private readonly clienteRepository: Repository<Cliente>,
+  ) { }
+
+  async create(createClienteDto: ClienteDto) {
+    try {
+      let cliente: Cliente = await this.clienteRepository.save(new Cliente(createClienteDto.nombre, createClienteDto.telefono, createClienteDto.dni));
+      if (cliente)
+        return cliente;
+      else
+        throw new Error("No se pudo crear cliente");
+    }
+    catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Error en la creacion de cliente' + error
+      }, HttpStatus.NOT_FOUND);
+    }
+  };
+
+  async findAll() {
+    let allClientes: Cliente[] = await this.clienteRepository.find();
+    return allClientes;
   }
 
-  findAll() {
-    return `This action returns all cliente`;
+  async findOne(id: number) {
+    try {
+      const filter: FindOneOptions = { where: { id: id } };
+      let cliente: Cliente = await this.clienteRepository.findOne(filter);
+      if (cliente)
+        return cliente;
+      else
+        throw new Error("Error en busqueda de id: " + id);
+    }
+    catch (error) {
+      throw new HttpException(
+        { status: HttpStatus.NOT_FOUND, error: `${error}` },
+        HttpStatus.NOT_FOUND
+      );
+    }
+  };
+
+  async update(id: number, updateClienteDto: ClienteDto) {
+    try {
+      let criterio: FindOneOptions = { where: { id: id } };
+      let cliente: Cliente = await this.clienteRepository.findOne(criterio);
+      if (!cliente)
+        throw new Error('No se encuentra el cliente');
+      else
+        cliente.setNombre(updateClienteDto.nombre);
+      cliente.setTelefono(updateClienteDto.telefono);
+      cliente.setDni(updateClienteDto.dni);
+
+      cliente = await this.clienteRepository.save(cliente);
+      return cliente;
+    } catch (error) {
+      throw new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'Error en la actiualizacion de cliente ' + error
+      }, HttpStatus.NOT_FOUND);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cliente`;
-  }
-
-  update(id: number, updateClienteDto: UpdateClienteDto) {
-    return `This action updates a #${id} cliente`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} cliente`;
+  public async remove(id: number): Promise<Boolean> {
+    try {
+      const criterio: FindOneOptions = { where: { id: id } }
+      let ciudad = await this.clienteRepository.findOne(criterio)
+      if (!ciudad) {
+        throw new Error(`No se pudo encontrar id: ` + id)
+      }
+      else {
+        await this.clienteRepository.delete(id)
+      }
+      return true
+    }
+    catch (error) {
+      throw new HttpException(
+        { status: HttpStatus.NOT_FOUND, error: `${error}` },
+        HttpStatus.NOT_FOUND
+      )
+    }
   }
 }
